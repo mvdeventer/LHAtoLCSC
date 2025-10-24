ee2a03f11f78489703156b94d5405b600840d28f
mvdeventer <mvdeventer@lhar.co.za>
2025-10-24 10:07:52 +0200

    Release v0.2.14

--- src/lhatolcsc/core/config.py
+++ src/lhatolcsc/core/config.py
@@ -29,7 +29,7 @@ class Config:
 
         # Application metadata
         self.app_name = "LHAtoLCSC"
-        self.version = "0.2.13"
+        self.version = "0.2.14"
 
         # API Configuration
         self.lcsc_api_key = os.getenv("LCSC_API_KEY", "")
